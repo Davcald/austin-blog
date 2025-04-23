25f4e805edcbfc4c844101c1b2575ac1e066ee04
David Caldwell <davidcaldwell@Davids-Mac-mini.attlocal.net>
2025-04-22 22:53:56 -0500

    Fix GitHub Pages deployment configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://davcald.github.io/austin-blog',
+	site: 'https://davcald.github.io',
+	base: '/austin-blog',
 	integrations: [mdx(), sitemap()],
 });
